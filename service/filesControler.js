515523be2febfb8b1d5cb682244393583e69fca5
adartal <82291327+adartal@users.noreply.github.com>
2024-03-14 16:59:19 +0200

    Update filesControler.js

--- service/filesControler.js
+++ service/filesControler.js
@@ -1,39 +1,26 @@
 const NodeCache = require('node-cache');
+const { spawn } = require('child_process');
 
 const cache = new NodeCache();
-async function upLoadFile(email, file) {
-  // Assuming the analysis is done here and we simply return a message
-  // TODO: replace this with actual analysis logic
-  return `Obfuscation Analysis Report
 
-Thank you for submitting your file for our obfuscation detection analysis. We have completed the examination of your code, and here are the results:
+function upLoadFile(email, file, callback) {
+  const python_process = spawn('python3', ['service/runModel.py']);
+  console.log("run model");
 
-Detection Summary:
+  let result = ''; // Accumulate the data
 
-- Obfuscated Code Detected: Yes
-- Confidence Level: 95%
-- Number of Checks Performed: 100
-- Number of Obfuscations Found: 95
+  python_process.stdout.on('data', (data) => {
+    const newData = data.toString();
+    console.log(newData);
+    result += newData; // Accumulate the data
+  });
 
-Details:
-Our analysis has identified patterns and techniques commonly associated with code obfuscation. The detected obfuscation techniques include:
-
-- String Encoding
-- Variable Renaming
-
-Recommendations:
-If the presence of obfuscated code is unexpected or unauthorized, we recommend a thorough review of your codebase. Obfuscated code can sometimes be an indicator of malicious intent or may simply be a method to protect intellectual property. If this analysis was for security purposes, consider consulting with a cybersecurity expert to understand the implications of the findings.
-
-
-Thank you for using our Obfuscation Detection Service.`;
+  // When the process ends, call the callback with the accumulated data
+  python_process.on('close', (code) => {
+    callback(result);
+  });
 }
 
-
-
-
-  module.exports = {
-    upLoadFile,
-  };
-  
-  
-  
+module.exports = {
+  upLoadFile,
+};
