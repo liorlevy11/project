89bc0a91ec0a9a7f640923e96e589519aee5ba73
Lior Levy <liolevy@cisco.com>
2024-06-08 19:30:18 +0300

    fix bugs

--- src/components/_tests_/Login.test.js
+++ src/components/_tests_/Login.test.js
@@ -2,7 +2,6 @@
 import '@testing-library/jest-dom';
 import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from "react-router-dom";
 import Login from "../Login";
 
